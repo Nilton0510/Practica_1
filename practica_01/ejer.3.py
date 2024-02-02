98d0357d0485945b0229fae628471ebdb4f4b326
“Nilton0510 <herreranilton15@gmail.com>
2024-02-01 21:49:52 -0500

    Practica_01

---
+++ practica_01/ejer.3.py
@@ -0,0 +1,18 @@
+
+diccionario = {
+    1: "Nombre : Nilton joseph",
+    2: "Apellidos :Lizana Herrera",
+3: "Edad : 22",
+4: "dni : 75651687"
+}
+print(diccionario)
+
+del diccionario[4]
+print(diccionario)
+
+diccionario ["Profesion"] = "Economista"
+print(diccionario)
+
+
+
+
